bffb1cf739abd90c6a501a96e4bdf8ef17a386a0
isunitos <sofiiguajardo0606@gmail.com>
2025-07-15 19:49:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+document.querySelectorAll('.course').forEach(course => {
+  course.addEventListener('click', () => {
+    alert(`Curso: ${course.textContent}\nCréditos: ${course.title}`);
+  });
+});
 
